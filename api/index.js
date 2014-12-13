9a5102ed6dda704817a78047ef343caf3c19febc
Ashley Williams <ashley@bocoup.com>
2014-12-13 11:44:20 -0500

    refactor to use express.Router

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const namespace = '/api/0/';
+const namespace = '/api/0';
 
 app.use(cors());
 
@@ -11,12 +11,16 @@ const BODY_LIMIT = '500kb';
 app.use(bodyParser.json({ limit:BODY_LIMIT }));
 app.use(bodyParser.urlencoded({ limit: BODY_LIMIT, extended: true }));
 
-app.get(namespace, function(req, res){
+const router = express.Router();
+
+router.get('/', function(req, res){
   res.send('welcome to unacademic-api');
 });
 
-app.get('*', function(req, res){
-  res.redirect(namespace);
+app.use(namespace, router);
+
+app.get('*', function(req,res){
+  res.redirect(namespace+'/');
 });
 
 module.exports = app; 
