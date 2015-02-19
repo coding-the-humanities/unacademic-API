98d5f5481302003af20228e1d19e6ad27d977ed5
Marijn Koolen <marijn.koolen@gmail.com>
2015-02-19 13:38:49 +0100

    Update routes to include user scope
    - routes now include user scope, so waypoints and constellations can be
      access by user and title
    - routes with ids are now scoped under '/id' to make explicit that these
      are requests by id

--- api/modules/waypoints/routes.js
+++ api/modules/waypoints/routes.js
@@ -5,10 +5,8 @@ var controller = require('./controller');
 var router = express.Router();
 
 router.post('/', function(req,res, next) {
-	console.log(typeof(Waypoint));
-	//var waypoint = new Waypoint(req.body);
-	var waypoint = req.body;
 	console.log('posting waypoint:');
+	var waypoint = req.body;
 	var savePromise = controller.saveWaypoint(waypoint)
 		.then(function(savedWaypoint) {
 			return res.status(200).json(savedWaypoint);
@@ -21,25 +19,47 @@ router.post('/', function(req,res, next) {
 
 router.get('/', function(req, res, next) {
 	console.log('getting all waypoints');
-	controller.getWaypoints().then(function(points) {
-		return res.json(points);
+	var query = {};
+	controller.getWaypoints(query).then(function(waypoints) {
+		return res.json(waypoints);
 	});
 });
 
-router.get('/:id', function(req, res, next) {
-	console.log('getting waypoint ' + req.params.id);
-	var waypointId = req.params.id;
-	var getPromise = controller.getWaypoint(waypointId).then(function(point) {
-		return res.status(200).json(point);
+router.get('/user/:user', function(req, res, next) {
+	var waypointUser = req.params.user;
+	console.log('getting all waypoints for user ' + waypointUser);
+	var query = {'curator': waypointUser};
+	controller.getWaypoints(query).then(function(waypoints) {
+		return res.json(waypoints);
+	});
+});
+
+router.get('/id/:id', function(req, res, next) {
+	console.log('getting waypoint with id ' + req.params.id);
+	var query = {'_id': req.params.id};
+	var getPromise = controller.getWaypoint(query).then(function(waypoint) {
+		return res.status(200).json(waypoint);
 	});
 	getPromise.catch(function(error) {
 		return res.status(500).send(error);
 	});
 });
 
-router.put('/:id', function(req, res, next) {
+router.get('/user/:user/title/:title', function(req, res, next) {
+	console.log('getting waypoint of user ' + req.params.user + ' with title ' + req.params.title);
+	var query = {'title': req.params.title, 'curator': req.params.user};
+	var getPromise = controller.getWaypoint(query).then(function(waypoint) {
+		return res.status(200).json(waypoint);
+	});
+	getPromise.catch(function(error) {
+		return res.status(500).send(error);
+	});
+});
+
+router.put('/id/:id', function(req, res, next) {
 	console.log('updating waypoint ' + req.params.id);
-	var updatePromise = controller.updateWaypoint(req.params.id, req.body)
+	var query = {'_id': req.params.id};
+	var updatePromise = controller.updateWaypoint(query, req.body)
 		.then(function(updatedWaypoint) {
 			return res.status(200).json(updatedWaypoint);
 		});
@@ -49,12 +69,42 @@ router.put('/:id', function(req, res, next) {
 	});
 });
 
-router.delete('/:id', function(req, res, next) {
-	console.log('deleting waypoint ' + req.params.id);
-	var waypointId = req.params.id;
-	Waypoint.findOneAndRemove({"_id": waypointId}, function(err, removed_point) {
+router.put('/user/:user/title/:title', function(req, res, next) {
+	var query = {
+		'curator': req.params.user,
+		'title': req.params.title
+	};
+
+	console.log('updating waypoint of user ' + query.curator + ' with title ' + query.title);
+	var updatePromise = controller.updateWaypoint(query, req.body)
+		.then(function(updatedWaypoint) {
+			return res.status(200).json(updatedWaypoint);
+		});
+
+	updatePromise.catch(function(error) {
+		return res.status(500).send(error.message);
+	});
+});
+
+router.delete('/id/:id', function(req, res, next) {
+	console.log('deleting waypoint with id ' + req.arams.id);
+	var query = {"_id": req.params.id};
+	Waypoint.findOneAndRemove(query, function(err, removed_waypoint) {
 		if (err) { next(err); }
-		res.json(removed_point);
+		res.json(removed_waypoint);
+	});
+});
+
+router.delete('/user/:user/title/:title', function(req, res, next) {
+	var query = {
+		'curator': req.params.user,
+		'title': req.params.title
+	};
+	console.log('deleting waypoint of user ' + query.curator + ' with title ' + query.title);
+
+	Waypoint.findOneAndRemove(query, function(err, removed_waypoint) {
+		if (err) { next(err); }
+		res.json(removed_waypoint);
 	});
 });
 
